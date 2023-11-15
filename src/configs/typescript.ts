cb4f42124b54f7d41c13f0327d2e497c8458d10e
Will Stone <654103+will-stone@users.noreply.github.com>
2023-11-15 20:12:52 +0000

    Support new rules

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -115,6 +115,7 @@ export function typescript(): Linter.FlatConfig[] {
         '@typescript-eslint/no-unsafe-enum-comparison': 'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/no-unsafe-unary-minus': 'off',
         '@typescript-eslint/non-nullable-type-assertion-style': 'off',
         '@typescript-eslint/prefer-destructuring': 'off',
         '@typescript-eslint/prefer-includes': 'off',
