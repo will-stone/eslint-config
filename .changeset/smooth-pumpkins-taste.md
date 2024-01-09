5c9f3dc48f95b83fa5e81f87e6835d684c9ed39d
Will Stone <654103+will-stone@users.noreply.github.com>
2024-01-09 15:27:18 +0000

    Upgrade all deps and support unused rules

---
+++ .changeset/smooth-pumpkins-taste.md
@@ -0,0 +1,5 @@
+---
+'@will-stone/eslint-config': minor
+---
+
+Support `unicorn/no-unnecessary-polyfills`.
--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -135,6 +135,7 @@ export function typescript(options?: Options): Linter.FlatConfig[] {
               '@typescript-eslint/no-unsafe-member-access': 'off',
               '@typescript-eslint/no-unsafe-return': 'off',
               '@typescript-eslint/no-unsafe-unary-minus': 'off',
+              '@typescript-eslint/no-useless-template-literals': 'error',
               '@typescript-eslint/non-nullable-type-assertion-style': 'off',
               '@typescript-eslint/prefer-destructuring': 'off',
               '@typescript-eslint/prefer-includes': 'off',
