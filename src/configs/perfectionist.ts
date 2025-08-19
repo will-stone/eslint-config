9a3a2e1e205bc554b7cfd28bab636e1d8a5e3dfe
Will Stone <654103+will-stone@users.noreply.github.com>
2025-08-19 19:57:08 +0100

    ^ Sort Flux Standard Actions

--- src/configs/perfectionist.ts
+++ src/configs/perfectionist.ts
@@ -2,6 +2,27 @@ import type { TSESLint } from '@typescript-eslint/utils'
 
 import pluginPerfectionist from 'eslint-plugin-perfectionist'
 
+const sortOrder = [
+  // Flux standard actions.
+  {
+    customGroups: {
+      type: '^type$',
+      payload: '^payload$',
+      error: '^error$',
+      meta: '^meta$',
+    },
+    groups: ['type', 'payload', 'error', 'meta'],
+    useConfigurationIf: {
+      allNamesMatchPattern: '^type|payload|meta|error$',
+    },
+  },
+  // Fallback for everything else.
+  {
+    partitionByNewLine: true,
+    type: 'natural',
+  },
+] as const
+
 export function perfectionist(): TSESLint.FlatConfig.Config[] {
   return [
     {
@@ -10,22 +31,26 @@ export function perfectionist(): TSESLint.FlatConfig.Config[] {
         perfectionist: pluginPerfectionist,
       },
       rules: {
-        'perfectionist/sort-imports': 'warn',
+        'perfectionist/sort-array-includes': 'off',
+        'perfectionist/sort-classes': 'off',
+        'perfectionist/sort-decorators': 'off',
+        'perfectionist/sort-enums': 'off',
+        'perfectionist/sort-exports': 'off',
+        'perfectionist/sort-heritage-clauses': 'off',
+        'perfectionist/sort-imports': ['warn', { type: 'natural' }],
+        'perfectionist/sort-interfaces': 'off',
+        'perfectionist/sort-intersection-types': 'off',
+        'perfectionist/sort-jsx-props': 'off',
+        'perfectionist/sort-maps': 'off',
+        'perfectionist/sort-modules': 'off',
+        'perfectionist/sort-named-exports': 'off',
         'perfectionist/sort-named-imports': 'warn',
-        'perfectionist/sort-object-types': [
-          'warn',
-          {
-            partitionByNewLine: true,
-            type: 'natural',
-          },
-        ],
-        'perfectionist/sort-objects': [
-          'warn',
-          {
-            partitionByNewLine: true,
-            type: 'natural',
-          },
-        ],
+        'perfectionist/sort-object-types': ['warn', ...sortOrder],
+        'perfectionist/sort-objects': ['warn', ...sortOrder],
+        'perfectionist/sort-sets': 'off',
+        'perfectionist/sort-switch-case': 'off',
+        'perfectionist/sort-union-types': 'off',
+        'perfectionist/sort-variable-declarations': 'off',
       },
     },
   ]
