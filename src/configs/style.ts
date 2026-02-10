fc134f9a65497635610fde981e9fc7a6b51ea52e
Will Stone <654103+will-stone@users.noreply.github.com>
2026-02-10 19:53:33 +0000

    ~ Turn off new stylistic rule

--- src/configs/style.ts
+++ src/configs/style.ts
@@ -24,6 +24,7 @@ export function style(): TSESLint.FlatConfig.Config[] {
         '@stylistic/curly-newline': 'off',
         '@stylistic/dot-location': 'off',
         '@stylistic/eol-last': 'off',
+        '@stylistic/exp-jsx-props-style': 'off',
         '@stylistic/exp-list-style': 'off',
         '@stylistic/function-call-argument-newline': 'off',
         '@stylistic/function-call-spacing': 'off',
